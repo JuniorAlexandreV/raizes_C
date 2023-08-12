c205e3792bc4a183c8c2eebef6742bf4229546e5
Alexandre Junior <alexandrej620@gmail.com>
2023-08-12 18:55:42 -0300

    raizes_code

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <locale.h>
+
+
+
+
+
+
+int main(){
+
+
+setlocale(LC_ALL,"Portuguese_Brazil");
+
+
+float a,b,c;
+float raizA, raizB;
+float raizUnica;
+
+
+printf("Digite os coeficientes da equação para obter as raízes:\n\n");
+printf("[a]=");
+scanf("%f",&a);
+printf("[b]=");
+scanf("%f",&b);
+printf("[c]=");
+scanf("%f",&c);
+
+
+float delta = pow(b,2)-4*a*c;
+printf("o delta é : %0.2f\n\n", delta);
+
+
+if (a !=0 )
+{
+  if(delta < 0){
+    printf("Não existe raiz");
+  }
+  if (delta == 0)
+  {
+    printf("\nRaiz única\n");
+    raizUnica = -b/(2*a);
+    printf("A raiz é %0.2f", raizUnica);
+  }
+  if(delta > 0)
+  {
+    printf("Existem duas raízes reais e distintas\n");
+   
+    float raizDelta = sqrt(delta);
+
+
+    raizA = (-b + raizDelta)/(2*a);
+    raizB = (-b - raizDelta)/(2*a);
+   
+
+
+    printf("As raizes são :\n");
+    printf("Raiz a = %0.2f\n", raizA);
+    printf("Raiz b = %0.2f\n\n", raizB);
+
+
+
+
+  }
+   
+} else {
+  printf("Não é a equação de segundo grau!");
+}
+
+
+
+
+
+
+
+
+
+
+ 
+return 0;
+}
